9b1f58976094c875000fe070ebd7b83b0dfce65e
ljiljak <ljiljakaleksandar@gmail.com>
2018-05-11 21:02:40 +0200

    Z2

--- src/components/AppPosts.vue
+++ src/components/AppPosts.vue
@@ -1,12 +1,34 @@
 <template>
-	<div></div>
+	<div>
+		<ul>
+		 	<li v-for="(post, key) in posts" :key="post.id"> 
+		 	{{post.title}} - {{ post.text }} </li>
+		 	
+        </ul>
+
+	</div>
 
 </template>
+
 <script>
 
-	export default {
+import {posts} from '../services/Posts.js'
 
+export default {
+ data () {
+ 	return {
+ 		posts: []
+ 		}
+	},
+
+	created() {
+		posts.getAll() 
+		.then((response) => {
+			this.posts = response.data
+		})
 	}
 
+}
+	
 
 </script>
