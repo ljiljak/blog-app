758944094d956bf2c1973bb74b0806f4de571aec
ljiljak <ljiljakaleksandar@gmail.com>
2018-05-11 18:41:31 +0200

    Z1

---
+++ src/components/NavBar.vue
@@ -0,0 +1,24 @@
+<template>
+
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <router-link class="navbar-brand" to="/AppPosts">Blog II</router-link>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <router-link class="nav-link" to="/AppPosts">Posts</router-link>
+      </li>
+    </ul>
+  </div>
+</nav>
+	
+
+</template>
+<script>
+
+	export default {
+}
+
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import Posts from './components/AppPosts.vue'
 
 Vue.config.productionTip = false
 
+Vue.use(VueRouter)
+
+const routes = [
+  { path: '/', redirect: '/AppPosts' },
+  { path: '/AppPosts', component: Posts }
+]
+
+const router = new VueRouter({
+  routes,
+  mode: 'history'
+})
+
 new Vue({
+	router,
   render: h => h(App)
 }).$mount('#app')
